f485885962b3c632ed769903564c2d48a4d00d67
Edan kinderman <62880315+idankinderman@users.noreply.github.com>
2023-07-03 11:44:05 +0300

    Update clinical_data_test.py

--- clinical_data_test.py
+++ clinical_data_test.py
@@ -81,113 +81,109 @@ def clinical_data_presentor(depth, path, net_name, input_size, dwi_input):
     plt.imshow(Fp, cmap="gray")
     
 # end of functions
-    
-    
-# define variables
-ind = 15
-sx = 156
-sy = 192
-clinical_b_values_numpy = clinical_b_values.numpy().astype(int)
-sb = len(clinical_b_values)
-dwi_image = np.zeros((sx,sy,sb))
-dwi_image_long = np.zeros((sx*sy,sb))
-dwi_image_for_nets = np.zeros((sx*sy,sb))
 
-# loading the clinical data
-reader = sitk.ImageFileReader()
-reader.SetImageIO("VTKImageIO")
-for i, b_value in enumerate(clinical_b_values_numpy):
-    reader.SetFileName('clinical_data_MDDW_UPPER_AVERAGED/b{}_averaged.vtk'.format(b_value))
-    image = reader.Execute()
-    Im = sitk.GetArrayFromImage(image)
-    dwi_image[:,:,i] = Im[ind,:,:]
-
-# calculates the image approximated snr for the snr input net - 2 options for each index
-# for index 15
-clinical_SNR = image_snr_calculater(dwi_image[:,:,0],37,40,57,60)
-#clinical_SNR = image_snr_calculater(dwi_image[:,:,0],82,85,121,124)
-
-# for index 24
-#clinical_SNR = image_snr_calculater(dwi_image[:,:,0],77,80,90,93)
-#clinical_SNR = image_snr_calculater(dwi_image[:,:,0],91,94,52,55)
-
-# using mask to leave only useful data
-mask = (dwi_image[:,:,0] < 40)
-for i in np.arange(sb):
-    masked_image = np.ma.array(dwi_image[:,:,i], mask = mask)
-    dwi_image[:,:,i] = masked_image.filled(fill_value=0)
-
-"""
-# present dwi image if needed
-plt.figure()
-plt.title("DWI MRI image with b=150")
-plt.imshow(dwi_image[:,:,6], cmap="gray")
-"""
-
-# the input for the nets
-dwi_image_long = np.reshape(dwi_image, (sx*sy, sb))
-
-# normalize the data for nets
-S0_vector = dwi_image_long[:,0]
-mask_vector = (S0_vector <= 0)
-masked_vector = np.ma.array(S0_vector, mask = mask_vector)
-S0_vector = masked_vector.filled(fill_value=1)
-for i in np.arange(sb):
-    dwi_image_for_nets[:,i] = dwi_image_long[:,i] / S0_vector
-
-
-
-# Classic solver
-dwi_image_classic_solver = np.zeros((sb,sx,sy)) #calssic solver needs shape (b_len,size_x,size_y)
-for i in np.arange(sb):
-    dwi_image_classic_solver[i,:,:] = dwi_image[:,:,i]
-S0_Mat = dwi_image_classic_solver[0,:,:]
-F_classic_solver, D_classic_solver, Dp_classic_solver = Least_Square(dwi_image_classic_solver, S0_Mat, clinical_b_values, sx, sy)
-
-
-# for ind = 15
-print("Classic solver")
-print("The Dp normalized std is:", normalized_std_calculater(Dp_classic_solver,40,43,95,98))
-print("The Dt normalized std is:", normalized_std_calculater(D_classic_solver,82,85,121,124))
-print("The Fp normalized std is:", normalized_std_calculater(F_classic_solver,55,58,66,69))
-print()
-
-"""
-# for ind = 24
-print("Classic solver")
-print("The Dp normalized std is:", normalized_std_calculater(Dp_classic_solver,91,94,121,124))
-print("The Dt normalized std is:", normalized_std_calculater(D_classic_solver,91,94,52,55))
-print("The Fp normalized std is:", normalized_std_calculater(F_classic_solver,52,55,111,114))
-print()
-"""
-
-plt.figure()
-plt.title("Classic Solver Dp")
-plt.imshow(Dp_classic_solver, cmap="gray")
-
-plt.figure()
-plt.title("Classic Solver D")
-plt.imshow(D_classic_solver, cmap="gray")
-
-plt.figure()
-plt.title("Classic Solver Fp")
-plt.imshow(F_classic_solver, cmap="gray")
-
-
-# The Basic net
-clinical_data_presentor(basic_depth, clinical_basic_path, 'Basic net', sb, dwi_image_for_nets)
-
-# The SNR net
-clinical_data_presentor(vars_depth, clinical_vars_path, 'SNR net', sb, dwi_image_for_nets)
-
-# The SNR with input net
-# adding the SNR as another input
-dwi_image_long_snr_input = np.zeros((sx*sy, sb+1))
-dwi_image_long_snr_input[:,0:sb] = dwi_image_for_nets
-dwi_image_long_snr_input[:,sb] = clinical_SNR/100
-
-clinical_data_presentor(vars_input_depth, clinical_vars_input_path, 'SNR with input net', sb+1, dwi_image_long_snr_input)
-
-
-
-
+if __name__ == '__main__':
+  # define variables
+  ind = 15
+  sx = 156
+  sy = 192
+  clinical_b_values_numpy = clinical_b_values.numpy().astype(int)
+  sb = len(clinical_b_values)
+  dwi_image = np.zeros((sx,sy,sb))
+  dwi_image_long = np.zeros((sx*sy,sb))
+  dwi_image_for_nets = np.zeros((sx*sy,sb))
+  
+  # loading the clinical data
+  reader = sitk.ImageFileReader()
+  reader.SetImageIO("VTKImageIO")
+  for i, b_value in enumerate(clinical_b_values_numpy):
+      reader.SetFileName('clinical_data_MDDW_UPPER_AVERAGED/b{}_averaged.vtk'.format(b_value))
+      image = reader.Execute()
+      Im = sitk.GetArrayFromImage(image)
+      dwi_image[:,:,i] = Im[ind,:,:]
+  
+  # calculates the image approximated snr for the snr input net - 2 options for each index
+  # for index 15
+  clinical_SNR = image_snr_calculater(dwi_image[:,:,0],37,40,57,60)
+  #clinical_SNR = image_snr_calculater(dwi_image[:,:,0],82,85,121,124)
+  
+  # for index 24
+  #clinical_SNR = image_snr_calculater(dwi_image[:,:,0],77,80,90,93)
+  #clinical_SNR = image_snr_calculater(dwi_image[:,:,0],91,94,52,55)
+  
+  # using mask to leave only useful data
+  mask = (dwi_image[:,:,0] < 40)
+  for i in np.arange(sb):
+      masked_image = np.ma.array(dwi_image[:,:,i], mask = mask)
+      dwi_image[:,:,i] = masked_image.filled(fill_value=0)
+  
+  """
+  # present dwi image if needed
+  plt.figure()
+  plt.title("DWI MRI image with b=150")
+  plt.imshow(dwi_image[:,:,6], cmap="gray")
+  """
+  
+  # the input for the nets
+  dwi_image_long = np.reshape(dwi_image, (sx*sy, sb))
+  
+  # normalize the data for nets
+  S0_vector = dwi_image_long[:,0]
+  mask_vector = (S0_vector <= 0)
+  masked_vector = np.ma.array(S0_vector, mask = mask_vector)
+  S0_vector = masked_vector.filled(fill_value=1)
+  for i in np.arange(sb):
+      dwi_image_for_nets[:,i] = dwi_image_long[:,i] / S0_vector
+  
+  
+  
+  # Classic solver
+  dwi_image_classic_solver = np.zeros((sb,sx,sy)) #calssic solver needs shape (b_len,size_x,size_y)
+  for i in np.arange(sb):
+      dwi_image_classic_solver[i,:,:] = dwi_image[:,:,i]
+  S0_Mat = dwi_image_classic_solver[0,:,:]
+  F_classic_solver, D_classic_solver, Dp_classic_solver = Least_Square(dwi_image_classic_solver, S0_Mat, clinical_b_values, sx, sy)
+  
+  
+  # for ind = 15
+  print("Classic solver")
+  print("The Dp normalized std is:", normalized_std_calculater(Dp_classic_solver,40,43,95,98))
+  print("The Dt normalized std is:", normalized_std_calculater(D_classic_solver,82,85,121,124))
+  print("The Fp normalized std is:", normalized_std_calculater(F_classic_solver,55,58,66,69))
+  print()
+  
+  """
+  # for ind = 24
+  print("Classic solver")
+  print("The Dp normalized std is:", normalized_std_calculater(Dp_classic_solver,91,94,121,124))
+  print("The Dt normalized std is:", normalized_std_calculater(D_classic_solver,91,94,52,55))
+  print("The Fp normalized std is:", normalized_std_calculater(F_classic_solver,52,55,111,114))
+  print()
+  """
+  
+  plt.figure()
+  plt.title("Classic Solver Dp")
+  plt.imshow(Dp_classic_solver, cmap="gray")
+  
+  plt.figure()
+  plt.title("Classic Solver D")
+  plt.imshow(D_classic_solver, cmap="gray")
+  
+  plt.figure()
+  plt.title("Classic Solver Fp")
+  plt.imshow(F_classic_solver, cmap="gray")
+  
+  
+  # The Basic net
+  clinical_data_presentor(basic_depth, clinical_basic_path, 'Basic net', sb, dwi_image_for_nets)
+  
+  # The SNR net
+  clinical_data_presentor(vars_depth, clinical_vars_path, 'SNR net', sb, dwi_image_for_nets)
+  
+  # The SNR with input net
+  # adding the SNR as another input
+  dwi_image_long_snr_input = np.zeros((sx*sy, sb+1))
+  dwi_image_long_snr_input[:,0:sb] = dwi_image_for_nets
+  dwi_image_long_snr_input[:,sb] = clinical_SNR/100
+  
+  clinical_data_presentor(vars_input_depth, clinical_vars_input_path, 'SNR with input net', sb+1, dwi_image_long_snr_input)
